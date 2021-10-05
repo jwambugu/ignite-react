0da29986e41abff541f5213822a2c32f336e2bf4
Joram Wambugu <artisan254@gmail.com>
2021-10-05 16:12:47 +0300

    Games
    Add global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Home from "./pages/Home";
+import GlobalStyles from "./components/GlobalStyles";
 
 const App = () => {
   return (
     <div className="App">
+      <GlobalStyles />
       <Home />
     </div>
   );
--- src/api.js
+++ src/api.js
@@ -6,10 +6,10 @@ const currentDate = getDate(0);
 const lastYear = getDate(-1);
 const nextYear = getDate(1);
 
-const popularGamesPath = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
-const upcomingGamesPath = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
-const newGamesPath = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
+// const popularGamesPath = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+// const upcomingGamesPath = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+// const newGamesPath = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
-export const popularGamesURL = `${baseURL}/${popularGamesPath}`;
-export const upcomingGamesURL = `${baseURL}/${upcomingGamesPath}`;
-export const newGamesURL = `${baseURL}/${newGamesPath}`;
+export const popularGamesURL = `${baseURL}/games/lists/popular?page_size=10`;
+export const upcomingGamesURL = `${baseURL}/games/lists/recent-games-future?ordering=-added&page_size=10`;
+export const newGamesURL = `${baseURL}/games/lists/recent-games?discover=true&ordering=-added&page_size=10`;
